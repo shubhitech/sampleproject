50d23755408d1bbed67000aab8b85b3d41d53e5f
shubhitech <shubhi171197gupta@gmail.com>
2023-07-25 18:20:52 +0530

    third commit

---
+++ Inspire.java
@@ -0,0 +1,13 @@
+
+public class Inspire
+{ 
+
+public static void main (String []args){ 
+int intValue = 100;
+char charValue = intValue; // Implicit typecasting
+
+System.out.println(charValue);
+
+}
+
+}
